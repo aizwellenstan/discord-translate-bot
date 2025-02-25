86403b0e37792baca1be7caf4165fcb111b9885a
aizwellenstan <aizwellenstan@gmail.com>
2025-02-25 15:00:08 +0900

    add create thread if @ bot else no reply

--- translate-bot.py
+++ translate-bot.py
@@ -92,9 +92,11 @@ async def on_message(message):
             # If it's in a thread, reply in the thread
             if message.thread:
                 await message.thread.send(content="\n".join(results))
+            """
             else:
                 # Otherwise, reply in the main channel
                 await message.reply(content="\n".join(results))
+            """
 
 keep_alive()
 try:
