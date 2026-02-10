415ffbe120ffc9a136323fd3395e07c03ed11ac2
私はただただ進むだけ <aizwellenstan@gmail.com>
2026-02-10 18:23:12 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-/home/ubuntu/discord-translate-bot/python3/bin/python3 translate-bot.py
+/opt/discord-translate-bot/venv/bin/python translate-bot.py
 echo "Starting my discord-translate-bot script" | logger
